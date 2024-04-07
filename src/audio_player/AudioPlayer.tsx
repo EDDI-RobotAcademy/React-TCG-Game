34f3fe6d66832b304f6ef0c9ba8af34dc5c376d5
silenc3502 <gcccompil3r@gmail.com>
2024-04-08 02:17:44 +0900

    update: battle field draw card

--- src/audio_player/AudioPlayer.tsx
+++ src/audio_player/AudioPlayer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AudioPlayer: React.FC<{ url: string }> = ({ url }) => {
     return (
-        <audio controls autoPlay style={{ display: 'none' }}>
+        <audio controls autoPlay loop style={{ display: 'none' }}>
             <source src={url} type="audio/mpeg" />
         </audio>
     );
