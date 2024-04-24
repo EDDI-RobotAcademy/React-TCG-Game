b0e6424e3d955a72210e7340f7f28f6ed1d2521a
Sanghoon Lee <gcccompil3r@gmail.com>
2024-04-24 17:51:18 +0900

    feat: kakao login finish

--- src/oauth/kakao/KakaoLogin.tsx
+++ src/oauth/kakao/KakaoLogin.tsx
@@ -1,12 +1,17 @@
-import { getImageUrl } from "../../aws/s3/aws_s3_utility";
-import { MouseEvent } from "react";
+// KakaoLogin.tsx
+
+import { Button, TextField } from "@mui/material";
+import { MouseEvent, ChangeEvent, useState } from "react";
 
 interface KakaoLoginProps {
     onSuccess: () => void;
-    email: string; // 추가
+    setEmail: (email: string) => void;
 }
 
-const KakaoLogin: React.FC<KakaoLoginProps> = ({ onSuccess, email }) => {
+const KakaoLogin: React.FC<KakaoLoginProps> = ({ onSuccess, setEmail }) => {
+    const [email, setEmailState] = useState("");
+    const [isValidEmail, setIsValidEmail] = useState(true); // 이메일 유효성 상태 추가
+
     const CLIENT_ID = `${process.env.REACT_APP_KAKAO_REST_API_KEY}`;
     const REDIRECT_URI = `${process.env.REACT_APP_KAKAO_REDIRECT_URL}`;
     const kakaoURL =
@@ -15,20 +20,36 @@ const KakaoLogin: React.FC<KakaoLoginProps> = ({ onSuccess, email }) => {
         `redirect_uri=${REDIRECT_URI}&` +
         "response_type=code";
 
-    const handleClick = (e: MouseEvent<HTMLDivElement>) => {
+    const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
         window.location.href = kakaoURL;
     };
 
+    const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const newEmail = e.target.value;
+        setEmailState(newEmail);
+        setEmail(newEmail);
+
+        // 이메일 유효성 검사
+        const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(newEmail);
+        setIsValidEmail(isValid);
+    };
+
     return (
-        <div className="oauth-login-btn-container" onClick={handleClick}>
-            <img
-                className="oauth-login-btn"
-                alt="KakaoLogin Icon"
-                // src={getImageUrl("resources/KakaoLoginIcon.png")}
+        <>
+            <TextField
+                fullWidth
+                label="이메일 주소"
+                variant="outlined"
+                value={email}
+                onChange={handleEmailChange}
+                error={!isValidEmail} // 이메일 유효성에 따라 에러 상태 설정
+                helperText={!isValidEmail && "유효한 이메일을 입력하세요"} // 에러 메시지 출력
+                style={{ marginBottom: 16 }}
             />
-            <p className="oauth-login-text">카카오 로그인</p>
-            <p>{email}</p> {/* 이메일 출력 */}
-        </div>
+            <Button variant="contained" onClick={handleClick} disabled={!email || !isValidEmail}>
+                카카오 로그인
+            </Button>
+        </>
     );
 };
 
